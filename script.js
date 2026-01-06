54c1757bb4103f0fcbef25a0e9628e4f04248e13
Raham Kalhoro <rahamkalhoro@gmail.com>
2026-01-06 15:19:12 +0500

    updated the html and css

--- script.js
+++ script.js
@@ -18,3 +18,44 @@ updateClock();
 
 // Set an interval to call updateClock() every 1000 milliseconds (1 second)
 setInterval(updateClock, 1000);
+
+//
+// Getting today items arrow
+const leftArrow = document.getElementById("leftArrow");
+const rightArrow = document.getElementById("rightArrow");
+const itemCards = document.getElementById("itemCards");
+
+// Scroll Left (Go back)
+rightArrow.addEventListener("click", () => {
+  itemCards.scrollBy({
+    left: -200, // Negative number moves content to the left
+    behavior: "smooth",
+  });
+});
+
+// Scroll Right (Go forward)
+leftArrow.addEventListener("click", () => {
+  itemCards.scrollBy({
+    left: 200, // Positive number moves content to the right
+    behavior: "smooth",
+  });
+});
+
+// Getting Category Cards
+const leftCategoryArrow = document.getElementById("leftArrow2");
+const rightCategoryArrow = document.getElementById("rightArrow2");
+const itemCategoriesDiv = document.getElementById("itemCategoriesDiv");
+
+leftCategoryArrow.addEventListener("click", () => {
+  itemCategoriesDiv.scrollBy({
+    left: -100,
+    behavior: "smooth",
+  });
+});
+
+rightCategoryArrow.addEventListener("click", () => {
+  itemCategoriesDiv.scrollBy({
+    left: 100,
+    behavior: "smooth",
+  });
+});
